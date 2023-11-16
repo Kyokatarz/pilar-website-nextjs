70a4143697e744a767c56c27f08f404c211e7341
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2023-11-16 21:16:25 +0200

    init nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Pilar Miralles · Composer - Sound Artist',
+  description:
+    "Hi, I'm Pilar Miralles - a Composer and Sound Artist. Welcome to my website!",
+  icons: ['/favicon.png'],
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en'>
+      <body>
+        <Navbar />
+        <main className='pt-14 min-h-screen'>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
