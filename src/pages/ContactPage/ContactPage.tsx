f3d3150299d63299f89a5efa1ea988d08447eb3d
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2024-01-21 23:45:44 +0200

    Migrating COntact page

---
+++ src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaFacebook } from 'react-icons/fa'
+import { ImSoundcloud2, ImGithub } from 'react-icons/im'
+import { IconContext } from 'react-icons'
+
+import Article from '@/components/Article'
+import Section from '@/components/Section'
+import Contact from '@/pages/HomePage/sections/Contact'
+
+const ContactPage = () => {
+  return <Contact />
+}
+
+export default ContactPage
