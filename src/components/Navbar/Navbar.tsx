b1ad4cecd882113371c9ca130b430b2946edb951
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2024-06-23 14:37:08 +0300

    Navbar hiding on scroll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 
 import icon from './assets/icon.svg'
 import DesktopNavLinks from './DesktopNavLinks'
 import MobileNavLinks from '../MobileNavLinks'
-// import DesktopNavLinks from './components/DesktopNavLinks'
-// import MobileNavLinks from './components/MobileNavLinks'
 
 const Navbar = ({ lang }: { lang: string }) => {
+  const navRef = useRef<HTMLElement>(null)
+  const previousPosition = useRef(0)
+
+  const changeNavbarVisibility = () => {
+    console.log('scroll')
+    const currentPosition = window.scrollY
+    const delta = previousPosition.current - currentPosition
+    const direction = delta < 0 ? 'down' : 'up'
+
+    if (direction === 'down' && scrollY > 200)
+      navRef.current?.classList.add('-top-20')
+    if (direction === 'up') navRef.current?.classList.remove('-top-20')
+
+    previousPosition.current = currentPosition
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNavbarVisibility)
+
+    return () => {
+      window.removeEventListener('scroll', changeNavbarVisibility)
+    }
+  }, [])
   return (
-    <nav className='w-full h-14 fixed flex flex-row items-center justify-between bg-white drop-shadow-md border-b-2 border-black'>
+    <nav
+      className='top-0 transition-all w-full h-14 fixed flex flex-row items-center justify-between bg-white drop-shadow-md border-b-2 border-black'
+      ref={navRef}
+    >
       <a href='/#landing-introduction'>
         <Image src={icon} alt='To home page icon' className='ml-4' />
       </a>
